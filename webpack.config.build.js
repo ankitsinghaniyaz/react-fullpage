27e0b316331e3f8a7a7546b652bd18e0252f32d6
Ankit Singhaniya <ankit.singhaniyaz@gmail.com>
2022-03-08 22:32:00 +0530

    Fix commonjs builds not working after webpack upgrade

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -28,7 +28,7 @@ module.exports = [
     output: {
       path: path.join(__dirname, 'dist'),
       filename: 'react-fullpage.js',
-      library: 'ReactFullpage',
+      // library: 'ReactFullpage',
       libraryTarget: 'commonjs2',
     },
   }),
